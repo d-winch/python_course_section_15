20f6fcd18c0c97e1d70b4d792642dc28e994f10e
winch_ <d.winchy@gmail.com>
2018-10-04 23:32:21 +0100

    Converted backend to a class

--- frontend.py
+++ frontend.py
@@ -14,7 +14,9 @@ Close
 """
 
 from tkinter import *
-import backend
+from backend import Database
+
+database = Database("books.db")
 
 def get_selected_row(event):
     try:
@@ -34,24 +36,24 @@ def get_selected_row(event):
 
 def view_command():
     list1.delete(0, END)
-    for row in backend.view():
+    for row in database.view():
         list1.insert(END, row)
 
 def search_command():
     list1.delete(0, END)
-    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+    for row in database.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
         list1.insert(END, row)
 
 def add_command():
-    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    database.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
     view_command()
 
 def delete_command():
-    backend.delete(selected_tuple[0])
+    database.delete(selected_tuple[0])
     view_command()
 
 def update_command():
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    database.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
     view_command()
 
 window = Tk()
